661aa668d589fd09f41e57057a0756089c5a0b0d
inoDevlog <ino0319@gmail.com>
2019-11-06 23:47:46 +0900

    test

--- src/main/java/polymorphism/LgTV.java
+++ src/main/java/polymorphism/LgTV.java
@@ -3,8 +3,12 @@ package polymorphism;
 import org.springframework.stereotype.Component;
 
 @Component("tv")
-public class LgTV implements TV{
-	
+public class LgTV implements TV {
+
+	public LgTV() {
+		System.out.println("===> LgTV °´Ã¼ »ý¼º");
+	}
+
 	public void powerOn() {
 		System.out.println("LgTV -- ON");
 	}
