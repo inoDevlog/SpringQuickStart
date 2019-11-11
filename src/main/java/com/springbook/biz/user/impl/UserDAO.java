b6be1effe06da4227840aa2f0b5e2f4b96d8d9e3
inoDevlog <ino0319@gmail.com>
2019-11-12 02:01:54 +0900

    annotation

--- src/main/java/com/springbook/biz/user/impl/UserDAO.java
+++ src/main/java/com/springbook/biz/user/impl/UserDAO.java
@@ -4,18 +4,25 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import org.springframework.stereotype.Repository;
+
 import com.springbook.biz.common.JDBCUtil;
 import com.springbook.biz.user.UserService;
 import com.springbook.biz.user.UserVO;
 
-public class UserDAO implements UserService {
+@Repository("userDAO")
+public class UserDAO{
 
+	// JDBC 관련 변수
 	private Connection conn = null;
 	private PreparedStatement stmt = null;
 	private ResultSet rs = null;
 
+	// SQL 명령어들
 	private final String USER_GET = "select * from users where id=? and password=?";
 
+	// CRUD 기능의 메소드 구현
+	// 회원 등록
 	public UserVO getUser(UserVO vo) {
 		UserVO user = null;
 		try {
