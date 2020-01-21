b99a6b9c3bff4502a9968d78b32edf303986b31b
choiinho <ino0319@gmail.com>
2020-01-21 23:54:36 +0900

    BoardDAO

--- src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -15,20 +15,20 @@ import com.springbook.biz.common.JDBCUtil;
 @Repository("boardDAO")
 public class BoardDAO {
 
-	// JDBC 관련 변수 
+	// JDBC 관련 변수
 	private Connection conn = null;
 	private PreparedStatement stmt = null;
 	private ResultSet rs = null;
 
-	// SQL 명령어들 
+	// SQL 명령어들
 	private final String BOARD_INSERT = "insert into board(seq, title, writer, content) values((select nvl(max(seq), 0)+1 from board),?,?,?)";
 	private final String BOARD_UPDATE = "update board set title=?, content=? where seq=?";
 	private final String BOARD_DELETE = "delete board where seq=?";
 	private final String BOARD_GET = "select * from board where seq=?";
 	private final String BOARD_LIST = "select * from board order by seq desc";
 
-	// CRUD 기능의 메소드 구현 
-	// 글 등록 
+	// CRUD 기능의 메소드 구현
+	// 글 등록
 	public void insertBoard(BoardVO vo) {
 		System.out.println("===> JDBC로 insertBoard() 기능 처리");
 		try {
@@ -45,7 +45,7 @@ public class BoardDAO {
 		}
 	}
 
-	// 글 수정 
+	// 글 수정
 	public void updateBoard(BoardVO vo) {
 		System.out.println("===> JDBC로 updateBoard() 기능 처리");
 		try {
@@ -62,7 +62,7 @@ public class BoardDAO {
 		}
 	}
 
-	// 글 삭제 
+	// 글 삭제
 	public void deleteBoard(BoardVO vo) {
 		System.out.println("===> JDBC로 deleteBoard() 기능 처리");
 		try {
@@ -77,7 +77,7 @@ public class BoardDAO {
 		}
 	}
 
-	// 글 상세 조회 
+	// 글 상세 조회
 	public BoardVO getBoard(BoardVO vo) {
 		System.out.println("===> JDBC로 getBoard() 기능 처리");
 		BoardVO board = null;
@@ -103,7 +103,7 @@ public class BoardDAO {
 		return board;
 	}
 
-	// 글 목록 조회 
+	// 글 목록 조회
 	public List<BoardVO> getBoardList(BoardVO vo) {
 		System.out.println("===> JDBC로 getBoardList() 기능 처리");
 		List<BoardVO> boardList = new ArrayList<BoardVO>();
