422be2a69d1aea6114539873de6135e8cbdb0543
choiinho <ino0319@gmail.com>
2020-02-06 23:16:31 +0900

    LogAdvice

--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,5 +1,6 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class LogAdvice {
@@ -9,10 +10,15 @@ public class LogAdvice {
 
 	}
 
-	@Pointcut("execution(* com.springbook.biz..*Impl.get*(..))")
-	public void getPointcut() {
-
+	@Before("allPointcut()")
+	public void printLog() {
+		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
 	}
+
+//	@Pointcut("execution(* com.springbook.biz..*Impl.get*(..))")
+//	public void getPointcut() {
+//
+//	}
 
 //	public void printLog(JoinPoint jp) {
 //		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
