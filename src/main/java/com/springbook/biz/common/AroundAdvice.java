827d6b3c2bb57d7bba83995a72c09d2e9d147c50
choiinho <ino0319@gmail.com>
2020-02-11 23:57:15 +0900

    AroundAdvice

--- src/main/java/com/springbook/biz/common/AroundAdvice.java
+++ src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -1,18 +1,21 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
 
 public class AroundAdvice {
-	
+
 	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable {
-		
+
 		String method = pjp.getSignature().getName();
-		
-		System.out.println("[Before]: 비즈니스 메소드 수행 전에 처리할 내용...");
-		
-		Object returnObj = pjp.proceed();
-		
-		System.out.println("[After]: 비즈니스 메소드 수행 후에 처리할 내용...");
-		return returnObj;
+
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+
+		Object obj = pjp.proceed();
+
+		stopWatch.stop();
+		System.out.println(method + "() 메소드 수행에 걸린 시간 : " + stopWatch.getTotalTimeMillis() + "(ms)");
+		return obj;
 	}
 }
