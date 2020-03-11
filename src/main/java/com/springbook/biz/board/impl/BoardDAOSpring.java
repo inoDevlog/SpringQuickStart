4fc507ebf89118023d4ec09018291af975a2bdc9
choiinho <ino0319@gmail.com>
2020-03-11 17:38:27 +0900

    클래스 수정

--- src/main/java/com/springbook/biz/board/impl/BoardDAOSpring.java
+++ src/main/java/com/springbook/biz/board/impl/BoardDAOSpring.java
@@ -1,8 +1,13 @@
 package com.springbook.biz.board.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
@@ -11,37 +16,41 @@ import com.springbook.biz.board.BoardVO;
 // DAO
 @Repository
 public class BoardDAOSpring extends JdbcDaoSupport {
-	
-	private final String BOARD_INSERT = "INSERT INTO BOARD(SEQ, TITLE, WRITER, CONTENT) VALUES((SELECT NVL(MAX(SEQ), 0)+1 FROM BOARD),?,?,?)";
-    private final String BOARD_UPDATE = "UPDATE BOARD SET TITLE=?, CONTENT=? WHERE SEQ=?";
-    private final String BOARD_DELETE = "DELETE BOARD WHERE SEQ=?";
-    private final String BOARD_GET = "SELECT * FROM BOARD WHERE SEQ=?";
-    private final String BOARD_LIST= "SELECT * FROM BOARD ORDER BY SEQ DESC";
-    
-    @Autowired
-    public void setCuperDataSource(DataSource dataSource) {
-    	super.setDataSource(dataSource);
-    }
-    
-    public void insertBoard(BoardVO vo) {
-    	System.out.println("insertBoard()");
-    	getJdbcTemplate().update(BOARD_INSERT, vo.getTitle(), vo.getWriter(), vo.getContent());
-    }
-    
-    public void updateBoard(BoardVO vo) {
-    	System.out.println("updateBoard()");
-    	getJdbcTemplate().update(BOARD_UPDATE, vo.getTitle(), vo.getContent(), vo.getSeq());
-    }
-    
-    public void deleteBoard(BoardVO vo) {
-    	System.out.println("deleteBoard()");
-    	getJdbcTemplate().update(BOARD_DELETE, vo.getSeq());
-    }
-    
-    public BoardVO getBoard(BoardVO vo) {
-    	System.out.println("getBoard()");
-    	Object[] args = {vo.getSeq()};
-    	return getJdbcTemplate().queryForObject(BOARD_GET, args, new BoardRowMapper());
-    }
 
+	private final String BOARD_INSERT = "INSERT INTO BOARD(SEQ, TITLE, WRITER, CONTENT) VALUES((SELECT NVL(MAX(SEQ), 0)+1 FROM BOARD),?,?,?)";
+	private final String BOARD_UPDATE = "UPDATE BOARD SET TITLE=?, CONTENT=? WHERE SEQ=?";
+	private final String BOARD_DELETE = "DELETE BOARD WHERE SEQ=?";
+	private final String BOARD_GET = "SELECT * FROM BOARD WHERE SEQ=?";
+	private final String BOARD_LIST = "SELECT * FROM BOARD ORDER BY SEQ DESC";
+
+	@Autowired
+	public void setCuperDataSource(DataSource dataSource) {
+		super.setDataSource(dataSource);
+	}
+
+	public void insertBoard(BoardVO vo) {
+		System.out.println("insertBoard()");
+		getJdbcTemplate().update(BOARD_INSERT, vo.getTitle(), vo.getWriter(), vo.getContent());
+	}
+
+	public void updateBoard(BoardVO vo) {
+		System.out.println("updateBoard()");
+		getJdbcTemplate().update(BOARD_UPDATE, vo.getTitle(), vo.getContent(), vo.getSeq());
+	}
+
+	public void deleteBoard(BoardVO vo) {
+		System.out.println("deleteBoard()");
+		getJdbcTemplate().update(BOARD_DELETE, vo.getSeq());
+	}
+
+	public BoardVO getBoard(BoardVO vo) {
+		System.out.println("getBoard()");
+		Object[] args = { vo.getSeq() };
+		return getJdbcTemplate().queryForObject(BOARD_GET, args, new BoardRowMapper());
+	}
+
+	public List<BoardVO> getBoardList(BoardVO vo) {
+		System.out.println("getBoardList()");
+		return getJdbcTemplate().query(BOARD_LIST, new BoardRowMapper());
+	}
 }
--- src/main/java/com/springbook/biz/board/impl/BoardRowMapper.java
+++ src/main/java/com/springbook/biz/board/impl/BoardRowMapper.java
@@ -8,12 +8,11 @@ import org.springframework.jdbc.core.RowMapper;
 import com.springbook.biz.board.BoardVO;
 
 public class BoardRowMapper implements RowMapper<BoardVO> {
-	
 	public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
 		
 		BoardVO board = new BoardVO();
-		board.setSeq(rs.getInt("SEQ"));
 		
+		board.setSeq(rs.getInt("SEQ"));
 		board.setTitle(rs.getString("TITLE"));
 		board.setWriter(rs.getString("WRITER"));
 		board.setContent(rs.getString("CONTENT"));
