97458bcf602caf93a4dbc081c77ff8e990ca0083
choiinho <ino0319@gmail.com>
2020-03-11 09:19:11 +0900

    jdbc 설

--- src/main/java/com/springbook/biz/board/impl/BoardDAOSpring.java
+++ src/main/java/com/springbook/biz/board/impl/BoardDAOSpring.java
@@ -3,6 +3,7 @@ package com.springbook.biz.board.impl;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import com.springbook.biz.board.BoardVO;
--- src/main/java/com/springbook/biz/board/impl/BoardRowMapper.java
+++ src/main/java/com/springbook/biz/board/impl/BoardRowMapper.java
@@ -3,11 +3,14 @@ package com.springbook.biz.board.impl;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import com.springbook.biz.board.BoardVO;
 import org.springframework.jdbc.core.RowMapper;
 
+import com.springbook.biz.board.BoardVO;
+
 public class BoardRowMapper implements RowMapper<BoardVO> {
+	
 	public BoardVO mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
 		BoardVO board = new BoardVO();
 		board.setSeq(rs.getInt("SEQ"));
 		
