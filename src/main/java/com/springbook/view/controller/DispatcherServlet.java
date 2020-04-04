571ee5bf66f5477e827bc6520798b2b775fda8e4
choiinho <ino0319@gmail.com>
2020-04-05 00:00:15 +0900

    process

--- src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -10,10 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public DispatcherServlet() {
-		super();
-	}
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		process(request, response);
@@ -21,10 +17,16 @@ public class DispatcherServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 		process(request, response);
 	}
 
 	private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		// 1. 클리이언트의 요청 path 정보를 추출한다.
+		String uri = request.getRequestURI();
+		String path = uri.substring(uri.lastIndexOf("/"));
+		System.out.println(path);
 
 	}
 
