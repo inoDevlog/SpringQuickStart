4b106e0ff77495b14dfed972a3a30145cb871e00
choiinho <ino0319@gmail.com>
2020-04-22 23:52:55 +0900

    BoardController

---
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -0,0 +1,52 @@
+package com.springbook.view.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAO;
+
+@Controller
+public class BoardController {
+
+	// 글 등록
+	@RequestMapping(value = "/insertBoard.do")
+	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
+
+		boardDAO.insertBoard(vo);
+		return "redirect:getBoardList.do";
+	}
+
+	@RequestMapping("/updateBoard.do")
+	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
+
+		boardDAO.updateBoard(vo);
+		return "getBoardList.do";
+	}
+
+	@RequestMapping("/deleteBoard.do")
+	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
+
+		boardDAO.deleteBoard(vo);
+		return "getBoardList.do";
+	}
+
+	@RequestMapping("/getBoard.do")
+	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+
+		mav.addObject("board", boardDAO.getBoard(vo)); // Model 정보 저장
+		mav.setViewName("getBoard.jsp"); // View 정보 저장
+		return mav;
+
+	}
+
+	@RequestMapping("/getBoardList.do")
+	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+
+		mav.addObject("boardList", boardDAO.getBoardList(vo)); // Model 정보 저장
+		mav.setViewName("getBoardList"); // View 정보 저장
+		return mav;
+	}
+
+}
