44c7477fe73d30d66da35d7bec4a9aad07f72c2f
choiinho <ino0319@gmail.com>
2020-04-24 17:25:34 +0900

    annotation 사용하기

--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -1,15 +1,28 @@
 package com.springbook.view.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
 public class BoardController {
-
+	
+	// 검색 조건 목록 설정
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap() {
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
+		return conditionMap;
+	}
+	
 	@RequestMapping(value = "/insertBoard.do")
 	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
 
@@ -32,19 +45,16 @@ public class BoardController {
 	}
 
 	@RequestMapping("/getBoard.do")
-	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
-
-		mav.addObject("board", boardDAO.getBoard(vo)); // Model 정보 저장
-		mav.setViewName("getBoard.jsp"); // View 정보 저장
-		return mav;
+	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
+		model.addAttribute("board", boardDAO.getBoard(vo)); // Model 정보 저장
+		return "getBoard.jsp";
 	}
 
 	@RequestMapping("/getBoardList.do")
-	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
 
-		mav.addObject("boardList", boardDAO.getBoardList(vo)); // Model 정보 저장
-		mav.setViewName("getBoardList.jsp"); // View 정보 저장
-		return mav;
+		model.addAttribute("boardList", boardDAO.getBoardList(vo));
+		return "getBoardList.jsp";
 	}
 
 }
