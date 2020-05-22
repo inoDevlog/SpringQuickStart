160413e2b3477e92d2e26b3bc4b49186b4d41738
choiinho <ino0319@gmail.com>
2020-05-22 23:14:58 +0900

    CommonExceptionHandler

---
+++ src/main/java/com/springbook/view/common/CommonExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.springbook.view.common;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice("com.springbook.view")
+public class CommonExceptionHandler {
+
+}
